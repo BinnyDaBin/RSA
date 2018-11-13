436d026cf1e8d4d0b60e47589576678a416e9a06
DA BIN LEE <dabin.lee@vistracks.com>
2018-11-12 21:25:08 -0500

    update1

--- src/RSA_main.java
+++ src/RSA_main.java
@@ -3,6 +3,33 @@ public class RSA_main {
 
 	public static void main(String[] args) {
 		// TODO hello Jen
+		System.out.println(encrypt("Hello World"));
 	}
-
+	
+	public static String encrypt(String message) {
+		int p = 11;
+		int q = 13;
+		int n = p*q;
+		int phi = (p-1)*(q-1);
+		int e = 23;
+		int d = 47;
+		
+		String intM = toASCII(message);
+		
+		return intM;
+	}
+	
+//	public static String decrypt(String decodedM) {
+//		
+//	}
+	
+	public static String toASCII(String message) {
+		int numL;
+		StringBuilder result = new StringBuilder();
+		for(char letter : message.toCharArray()) {
+			numL = (int) letter;
+			result.append(numL + " ");
+		}
+		return result.toString();
+	}
 }
