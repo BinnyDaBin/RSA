34f5ceea0fc0908d4852c64410e594d94e737db7
DA BIN LEE <dabin.lee@vistracks.com>
2018-11-13 21:44:09 -0500

    update2

--- src/RSA_main.java
+++ src/RSA_main.java
@@ -1,34 +1,46 @@
+import java.math.BigInteger;
 
 public class RSA_main {
 
 	public static void main(String[] args) {
 		// TODO hello Jen
-		System.out.println(encrypt("Hello World"));
+		System.out.println(encrypt("Hello Jen!"));
+		System.out.println(decrypt("1566 1342 1550 1550 1648 1544 169 1342 1614 1940 "));
 	}
 	
 	public static String encrypt(String message) {
-		int p = 11;
-		int q = 13;
-		int n = p*q;
+		int p = 43;
+		int q = 59;
+		BigInteger n = BigInteger.valueOf(p*q);
 		int phi = (p-1)*(q-1);
-		int e = 23;
-		int d = 47;
+		BigInteger e = new BigInteger("13");
+		BigInteger d = new BigInteger("937");
+		BigInteger numL;
+		BigInteger encL;
+		StringBuilder result = new StringBuilder();
 		
-		String intM = toASCII(message);
-		
-		return intM;
+		for(char letter : message.toCharArray()) {
+			numL = BigInteger.valueOf(letter);
+			encL = numL.modPow(e,n);
+			result.append(encL + " ");
+		}
+		return result.toString();
 	}
 	
-//	public static String decrypt(String decodedM) {
-//		
-//	}
-	
-	public static String toASCII(String message) {
-		int numL;
+	public static String decrypt(String encryptedM) {
+		BigInteger numL,decInt;
+		char decL;
+		BigInteger n = new BigInteger("2537");
+		// Find d
+		BigInteger d = new BigInteger("937");
 		StringBuilder result = new StringBuilder();
-		for(char letter : message.toCharArray()) {
-			numL = (int) letter;
-			result.append(numL + " ");
+		
+		String[] letters = encryptedM.split(" ");
+		for(String letter : letters) {
+			numL = BigInteger.valueOf(Integer.parseInt(letter));
+			decInt = numL.modPow(d, n);
+			decL = (char) decInt.intValue();
+			result.append(decL);
 		}
 		return result.toString();
 	}
